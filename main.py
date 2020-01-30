cfd32d1417890bf8e0cce6e0e80bb8fac3c4abc3
NaokiSato102 <s17c1066nq@s.chibakoudai.jp>
2020-01-30 17:16:08 +0900

    change class structure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # coding: UTF-8
 import datetime as dt
-
+import numpy as np
 
 """
 変数　略語集
@@ -22,70 +22,93 @@ class Total_EXP:
 		self.Lv_list = []
 	
 	def TE_List_read(self): # T.E = Total EXP
+		"""
+			Expリストを読み込む。
+			今後のLv上限拡張に対して柔軟に対応可能なように、
+			コードにべた書きでなく 別途テキストデータを読み込む形に。
+		"""
+
 		self.Lv_list = [int(line) for line in open('Total_EXP_List.txt')]
 
-	def TE_calc(self,Lv,RE):# R.E = remainingEXP
+	def TE_calc(self,lv,rem_exp):# R.E = remainingEXP
 		'''
-		Lv xを入力すると Lv x+1の累計EXP値が参照される。
-		それから「次のLvまであとどれくらいか」のEXP値を引くと
-		現在のEXPが求まる。
+			Lvと、次のレベルまでの残りEXPによって累計EXPを計算する。
+
+			Lv xを入力すると Lv x+1の累計EXP値が参照される。  
+			それから「次のLvまであとどれくらいか」のEXP値を引くと
+			現在のEXPが求まる。
+
+			Parameters
+			----------
+			lv : int
+				Lv
+			rem_exp : int
+				次のレベルまでの残りExp remaining exp
+
+			Return
+			-------
+			total_exp : int
+				累計Exp
 		'''
 
-		return self.Lv_list[Lv] - RE
+		return self.Lv_list[lv] - rem_exp
 
-class F_data:
-	def __init__(self, date, Lv, RE, diff_date, TE, PE, PLv):
-		self.date = date
-		self.Lv = Lv
-		self.RE = RE
-		self.diff_date =diff_date
-		self.TE = TE
-		self.PE = PE
-		self.PLv = PLv
+class FG_data:
+	def __init__(self):
+		self.name = ""
+		self.dst_lv = 0
+		self.dst_exp = 0
+		self.date = []
+		self.lv = []
+		self.rem_exp = []
+		self.total_exp = np.empty(0, dtype=np.int)
 
-#print(TE_calc(55,1056))
+
+
+
 
 TE = Total_EXP()
 TE.TE_List_read()
 
+
+
+one = FG_data()
 with open("sampledata.txt",encoding='utf-8') as f:
-	shipname = f.readline().strip("\n")
-	print('[{}]'.format(shipname) )
+	one.name = f.readline().strip("\n")
+	 
 
-	startdate_str = f.readline().strip("\n")
-	startdate = dt.datetime.strptime(startdate_str, '%y.%m.%d')
-	print('[{}]'.format(startdate) )
 
-	startLv = int(f.readline().strip("\n") )
-	print('[{}]'.format(startLv) )
-	datas =f.readlines()
-	F_datas = []
+	
+	one.dst_lv = int(f.readline().strip("\n") )
+	print('[{}]'.format(one.dst_lv) )
+
+	one.dst_exp = TE.TE_calc(one.dst_lv-1, 0)
+	print('[{}]'.format(one.dst_exp) )
+	
+	datas = f.readlines()
+	
 	for i in datas:
-		data_s = i.split()
-		print(len(data_s))
-		F_datas.append(
-			F_data(
-				dt.datetime.strptime(data_s[0], '%y.%m.%d'),
-				int( data_s[1] ),
-				int( data_s[2] ),
-				dt.datetime.strptime(data_s[0], '%y.%m.%d') - startdate ,
-				TE.TE_calc( int(data_s[1]), int(data_s[2]) ),
-				0,
-				0
-			)
-		) 
+		date, lv, rem_exp = i.split()
+
+		date = dt.datetime.strptime(date, '%y.%m.%d')
+		lv = int(lv)
+		rem_exp = int(rem_exp)
+
+		one.date = np.append(one.date, date)
+		one.lv.append(lv)
+		one.rem_exp.append(rem_exp)
+		one.total_exp = np.append(one.total_exp, TE.TE_calc(lv, rem_exp) )
 
 
 
-
-print("日付    \t Lv\t     RE\thoge  \t     TE")	
-for i in F_datas:
-	print("{}\t{:>3}\t{:>7}\t{:>2}\t{:>7}".format( 
-			i.date.strftime('%y.%m.%d'),
-			i.Lv,
-			i.RE,
-			int(i.diff_date.days),
-			i.TE
+print("日付    \t Lv\t     RE\t     TE")	
+for i in range( len(one.date) ):
+	print("{}\t{:>3}\t{:>7}\t{:>7}".format( 
+			one.date[i].strftime('%y.%m.%d'),
+			one.lv[i],
+			one.rem_exp[i],
+			one.total_exp[i]
 		)
 	)
-print(type(F_datas[0].date))
+
+
